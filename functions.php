5a30d581c6b7bebfd44a8abecbea4c42fcd6c46f
TranHuuAn-238 <tranhuuan2308@gmail.com>
2025-08-03 22:11:13 +0700

    cau truc thu muc theme wordpress

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+global $theme_prefix, $theme_uri;
+$theme_prefix   = 'wp2025_theme';
+$theme_uri      = get_template_directory_uri() . '/assets';
+$theme_version  = '1.0';
+
+// Đăng ký các thành phần hỗ trợ cho theme
+// Đăng ký style cho theme
+// Đăng ký javascript cho theme
