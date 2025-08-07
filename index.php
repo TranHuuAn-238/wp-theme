0e19880285d8342b4c5ef5a81388115acdcdbf04
TranHuuAn-238 <tranhuuan2308@gmail.com>
2025-08-07 21:29:33 +0700

    phan tach cac thanh phan giao dien

--- index.php
+++ index.php
@@ -2,21 +2,7 @@
     <?php get_header(); ?>
 
     <!-- Breadcrumb Section Begin -->
-    <section class="breadcrumb-section set-bg" data-setbg="<?= $theme_uri; ?>/img/breadcrumb.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="breadcrumb__text">
-                        <h2>Blog</h2>
-                        <div class="breadcrumb__option">
-                            <a href="./index.html">Home</a>
-                            <span>Blog</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php get_template_part('template-parts/page/breadcrumb'); ?>
     <!-- Breadcrumb Section End -->
 
     <!-- Blog Section Begin -->
