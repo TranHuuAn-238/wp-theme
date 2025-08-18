d75b69f8ab8205c889717cc6a2a27cc9bf92cc18
TranHuuAn-238 <tranhuuan2308@gmail.com>
2025-08-18 22:16:18 +0700

    lam viec voi trang tim kiem, category, tag

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <div class="blog__sidebar__search">
-    <form action="" method="GET">
+    <form action="<?= home_url(); ?>" method="GET">
         <input type="text" name="s" placeholder="Search...">
         <button type="submit"><span class="icon_search"></span></button>
     </form>
--- template-parts/page/breadcrumb.php
+++ template-parts/page/breadcrumb.php
@@ -1,13 +1,30 @@
 <?php global $theme_uri; ?>
+<?php
+    $page_title = 'Blog';
+    $page_sub_title = 'Blog';
+    if (is_category()) {
+        $page_title     = single_cat_title('Chuyên mục: ', false);
+        $page_sub_title = single_cat_title('', false);
+    }
+    if (is_tag()) {
+        $page_title     = single_cat_title('Thẻ: ', false);
+        $page_sub_title = single_cat_title('', false);
+    }
+    if (is_search()) {
+        $s = get_query_var('s');  // $_GET
+        $page_title     = 'Tìm kiếm cho: ' . $s;
+        $page_sub_title = 'Kết quả tìm kiếm cho ' . $s;
+    }
+?>
 <section class="breadcrumb-section set-bg" data-setbg="<?= $theme_uri; ?>/img/breadcrumb.jpg">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <div class="breadcrumb__text">
-                    <h2>Blog</h2>
+                    <h2><?= $page_title; ?></h2>
                     <div class="breadcrumb__option">
-                        <a href="./index.html">Home</a>
-                        <span>Blog</span>
+                        <a href="<?= home_url(); ?>">Home</a>
+                        <span><?= $page_sub_title; ?></span>
                     </div>
                 </div>
             </div>
